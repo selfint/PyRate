2e3106dc17e86e96fae2e566bd72be2154317023
Tom Selfin <selfint@gmail.com>
2021-03-13 15:03:31 +0200

    rename editor temp test file

--- tests/constants.py
+++ tests/constants.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 
 TEST_WORKING_DIRECTORY = Path("./test_temp_wd")
-TEST_TEMP_FILE = Path("test_editor_temp_file.py")
+TEST_EDITOR_TEMP_FILE = Path("test_editor_temp_file.py")
--- tests/test_editor.py
+++ tests/test_editor.py
@@ -2,21 +2,21 @@ import unittest
 
 from src.editor.editor import Editor
 
-from tests.constants import TEST_WORKING_DIRECTORY, TEST_TEMP_FILE
+from tests.constants import TEST_WORKING_DIRECTORY, TEST_EDITOR_TEMP_FILE
 
 
 class TestEditor(unittest.TestCase):
     def setUp(self) -> None:
         TEST_WORKING_DIRECTORY.mkdir(parents=True, exist_ok=True)
-        (TEST_WORKING_DIRECTORY / TEST_TEMP_FILE).touch(exist_ok=True)
+        (TEST_WORKING_DIRECTORY / TEST_EDITOR_TEMP_FILE).touch(exist_ok=True)
 
     def tearDown(self) -> None:
-        (TEST_WORKING_DIRECTORY / TEST_TEMP_FILE).unlink(missing_ok=True)
+        (TEST_WORKING_DIRECTORY / TEST_EDITOR_TEMP_FILE).unlink(missing_ok=True)
         TEST_WORKING_DIRECTORY.rmdir()
 
     def test_editor_can_edit_files(self):
         e = Editor(TEST_WORKING_DIRECTORY)
-        e.edit_file(TEST_TEMP_FILE)
+        e.edit_file(TEST_EDITOR_TEMP_FILE)
 
 
 if __name__ == "__main__":
