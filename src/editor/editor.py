de916adda5f48bebaa917df0a6ba43bb55bc9b82
Tom Selfin <selfint@gmail.com>
2021-03-13 13:17:18 +0200

    improve editor path handling
    use a working directory and only work on files
    that are inside that directory

--- src/editor/editor.py
+++ src/editor/editor.py
@@ -2,10 +2,32 @@ from pathlib import Path
 
 
 class Editor:
-    def __init__(self):
+    def __init__(self, working_directory: Path):
+        self._working_directory = working_directory.resolve()
         self._buffers = dict()
 
     def edit_file(self, file_path: Path) -> None:
         """Read file text into a buffer."""
 
-        self._buffers[file_path] = file_path.read_text()
+        local_file_path = self._get_local_file_path(file_path)
+
+        self._buffers[local_file_path] = local_file_path.read_text()
+
+    def _get_local_file_path(self, file_path: Path) -> Path:
+        """
+        Get path of file relative to working directory.
+
+        :raises FileNotFoundError: if file couldn't be found in working directory.
+        """
+
+        # check if file is not already relative to working directory
+        if self._working_directory not in file_path.parents:
+
+            # generate local file path if it exists
+            if not (local_file_path := self._working_directory / file_path).exists():
+                raise FileNotFoundError(
+                    f"couldn't find file {self._working_directory / file_path}"
+                )
+        else:
+            local_file_path = file_path
+        return local_file_path
--- tests/test_editor.py
+++ tests/test_editor.py
@@ -5,18 +5,21 @@ from src.editor.editor import Editor
 from pathlib import Path
 
 
-TEST_TEMP_FILE = Path("test_temp_file.py")
+TEST_WORKING_DIRECTORY = Path("./test_temp_wd")
+TEST_TEMP_FILE = Path("test_editor_temp_file.py")
 
 
 class TestEditor(unittest.TestCase):
     def setUp(self) -> None:
-        TEST_TEMP_FILE.touch(exist_ok=True)
+        TEST_WORKING_DIRECTORY.mkdir(parents=True, exist_ok=True)
+        (TEST_WORKING_DIRECTORY / TEST_TEMP_FILE).touch(exist_ok=True)
 
     def tearDown(self) -> None:
-        TEST_TEMP_FILE.unlink(missing_ok=True)
+        (TEST_WORKING_DIRECTORY / TEST_TEMP_FILE).unlink(missing_ok=True)
+        TEST_WORKING_DIRECTORY.rmdir()
 
     def test_editor_can_edit_files(self):
-        e = Editor()
+        e = Editor(TEST_WORKING_DIRECTORY)
         e.edit_file(TEST_TEMP_FILE)
 
 
