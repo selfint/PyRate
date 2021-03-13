7c1d5647bd234c4d175e012aa04122bc6d043e94
Tom Selfin <selfint@gmail.com>
2021-03-13 12:57:36 +0200

    extract testable user interface to helper directory

---
+++ tests/helpers/testable_user_interface.py
@@ -0,0 +1,37 @@
+from typing import List
+
+from src.interfaces.interface_device import InterfaceDevice
+from src.interfaces.user_interface import UserInterface
+
+
+class TestableInterfaceDevice(InterfaceDevice):
+    def __init__(self, inputs: List[str]):
+        self.inputs = inputs
+        self.index = 0
+        self.outputs = []
+        self.last_output = None
+
+    def get_input(self):
+        if self.index >= len(self.inputs):
+            raise Exception("Test input index got out of range - test failed")
+
+        value = self.inputs[self.index]
+        self.index += 1
+        return value
+
+    def send_output(self, msg: str):
+        self.outputs.append(msg)
+        self.last_output = msg
+
+    def get_output(self):
+        return self.last_output
+
+
+def helper_get_testable_user_interface(
+    stdin: List[str] = [], stdout: List[str] = [], stderr: List[str] = []
+):
+    return UserInterface(
+        TestableInterfaceDevice(stdin),
+        TestableInterfaceDevice(stdout),
+        TestableInterfaceDevice(stderr),
+    )
--- tests/test_user_interface.py
+++ tests/test_user_interface.py
@@ -1,41 +1,6 @@
 import unittest
-from typing import List
 
-from src.interfaces.user_interface import UserInterface
-from src.interfaces.interface_device import InterfaceDevice
-
-
-class TestableInterfaceDevice(InterfaceDevice):
-    def __init__(self, inputs: List[str]):
-        self.inputs = inputs
-        self.index = 0
-        self.outputs = []
-        self.last_output = None
-
-    def get_input(self):
-        if self.index >= len(self.inputs):
-            raise Exception("Test input index got out of range - test failed")
-
-        value = self.inputs[self.index]
-        self.index += 1
-        return value
-
-    def send_output(self, msg: str):
-        self.outputs.append(msg)
-        self.last_output = msg
-
-    def get_output(self):
-        return self.last_output
-
-
-def helper_get_testable_user_interface(
-    stdin: List[str] = [], stdout: List[str] = [], stderr: List[str] = []
-):
-    return UserInterface(
-        TestableInterfaceDevice(stdin),
-        TestableInterfaceDevice(stdout),
-        TestableInterfaceDevice(stderr),
-    )
+from tests.helpers.testable_user_interface import helper_get_testable_user_interface
 
 
 class TestUserInterface(unittest.TestCase):
